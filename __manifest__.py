19efcb20666a4c748a1f65fc8720cc0cc8e8c225
Paul Mbilong <paul.mbilong@gmail.com>
2025-12-10 15:46:54 +0100

    03 fichiers de sécurité

--- __manifest__.py
+++ __manifest__.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Real Estate', # Titre du module
-    'version': '1.0', # Version du module
-    'summary': 'Publicité immobilière', # Résumé du module
-    'installable': True,# Le module peut être installé
-    'author': 'Paul Mbilong', # Auteur du module
-    'category': 'Real Estate', # Catégorie du module
-    'depends': ['base'], # Dépendances du module
-    'application': True, # Le module est une application
+    "name": "Real Estate",  # Titre du module
+    "version": "1.0",  # Version du module
+    "summary": "Publicité immobilière",  # Résumé du module
+    "installable": True,  # Le module peut être installé
+    "author": "Paul Mbilong",  # Auteur du module
+    "category": "Real Estate",  # Catégorie du module
+    "depends": ["base"],  # Dépendances du module
+    "application": True,  # Le module est une application
+    "data": [
+        "security/estate_security.xml",  # 1️⃣ D'abord les groupes
+        "security/ir.model.access.csv",  # 2️⃣ Puis les droits d'accès
+        "views/estate_property_views.xml",  # 3️⃣ Enfin les vues
+    ],
 }
