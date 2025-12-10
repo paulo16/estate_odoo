e71b4d1ddc4887b71b2813276ef8b10cea3e6be8
Paul Mbilong <paul.mbilong@gmail.com>
2025-12-10 09:32:44 +0100

    les fichiers de bases à mettre en place

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Real Estate', # Titre du module
+    'version': '1.0', # Version du module
+    'summary': 'Publicité immobilière', # Résumé du module
+    'installable': True,# Le module peut être installé
+    'depends': ['base'], # Dépendances du module
+    'application': True, # Le module est une application
+}
